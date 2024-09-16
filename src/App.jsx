6594d7be12a68c9c67c754b5801338a45a4a344e
Tiberiu Badea <tcristian.b@gmail.com>
2024-09-16 11:16:43 +0100

    Issue #1:[WIP]: Add Header component with logo, add animation, add env for social, add custom css for animation, responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="min-h-screen bg-black flex flex-col top-0 text-white">
-      <h1 className="hover:text-red-500 text-center text-6xl p-4">
-        Code-Pages
-      </h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import LogoCode from "./LogoCode";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="flex flex-col p-2 md:p-4 bg-black">
+        <div className="flex flex-row mr-20 ml-0">
+          <a
+            href="/"
+            rel="noopener noreferrer"
+            className="text-white flex w-1/5 -ml-3 md:-ml-36 "
+          >
+            <LogoCode />
+          </a>
+        </div>
+        <div>
+          <p className="text-xs md:text-sm text-white text-right mt-2.5 md:-mt-10 ">
+            where [c] stands for more than just crypto
+          </p>
+        </div>
+      </header>
+      <div className="text-right text-white md:text-2xl text-sm  ">
+        <a href={import.meta.env.VITE_APP_BLOG_URL}>
+          <span className="md:mr-20 mr-10 hover:text-red-500">Blog</span>
+        </a>
+        <a href={import.meta.env.VITE_APP_X_URL}>
+          <span className="md:mr-20 mr-10 hover:text-red-500">Twitter/X</span>
+        </a>
+        <a href={import.meta.env.VITE_APP_DISCORD_HUB_URL}>
+          <span className="md:mr-20 mr-10 hover:text-red-500">
+            Discord-Research-Hub
+          </span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
