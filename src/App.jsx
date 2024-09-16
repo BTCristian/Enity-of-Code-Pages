73c88a8ff29bc2e56ef51998591f3a03ad363645
Tiberiu Badea <tcristian.b@gmail.com>
2024-09-16 11:21:38 +0100

    Issue #2:[WIP]: Add responsive footer with temporary content, ref to EntityC and link to env var, currently to deployed EntityC

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="min-h-screen bg-black flex flex-col top-0 text-white">
       <Header />
+      <Footer />
     </div>
   );
 }
