d4ea63d74f2f014d2c90f1a7ff8051395f13aa04
Tiberiu Badea <tcristian.b@gmail.com>
2024-09-16 11:02:57 +0100

    Issue #1:[WIP]: Build with Vite, JS, TailwindCSS, config and install packages, add content to readme, add favicon

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white text-5xl text-center p-6">
+      <h1 className="hover:text-red-500">Code-Pages</h1>
+    </div>
+  );
+}
+
+export default App;
