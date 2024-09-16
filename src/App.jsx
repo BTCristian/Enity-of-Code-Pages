1b7844ede1e8972c8d6e61c81166c42e88afe642
Tiberiu Badea <tcristian.b@gmail.com>
2024-09-16 11:24:50 +0100

    Issue #4:[WIP]: Add BlogSection component, integration with Odysee RSS feed for specific channel, channel set as environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import BlogSection from "./components/BlogSection";
 
 function App() {
   return (
     <div className="min-h-screen bg-black flex flex-col top-0 text-white">
       <Header />
+      <div className="p-3 mb-2">
+        <BlogSection />
+      </div>
       <Footer />
     </div>
   );
